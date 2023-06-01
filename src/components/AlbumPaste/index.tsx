283c7988b92ace774cf2cf1e85112f286b51783a
glauber2k2 <devglaubermonteiro@gmail.com>
2023-06-01 20:06:45 -0300

    add button backHome in Album

--- src/components/AlbumPaste/index.tsx
+++ src/components/AlbumPaste/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import styles from './AlbumPaste.module.css';
+import { House } from 'phosphor-react';
+import Link from 'next/link';
 
 interface Props {
   name: string;
@@ -7,29 +9,38 @@ interface Props {
 
 export default function AlbumPaste({ name }: Props) {
   return (
-    <div className={styles.container}>
-      <h1>{name}</h1>
-      <Image
-        src={`/album/${name}/${name}1.jpg`}
-        width={1280}
-        height={1280}
-        alt=''
-        className={styles.photoNumberOne}
-      />
-      <Image
-        src={`/album/${name}/${name}2.jpg`}
-        width={1280}
-        height={1280}
-        alt=''
-        className={styles.photoNumberTwo}
-      />
-      <Image
-        src={`/album/${name}/${name}3.jpg`}
-        width={1280}
-        height={1280}
-        alt=''
-        className={styles.photoNumberThree}
-      />
-    </div>
+    <>
+      <div className={styles.backHome}>
+        <Link href='/' legacyBehavior>
+          <a>
+            <House weight='fill' size={26} /> Inicio
+          </a>
+        </Link>
+      </div>
+      <div className={styles.container}>
+        <h1>{name}</h1>
+        <Image
+          src={`/album/${name}/${name}1.jpg`}
+          width={1280}
+          height={1280}
+          alt=''
+          className={styles.photoNumberOne}
+        />
+        <Image
+          src={`/album/${name}/${name}2.jpg`}
+          width={1280}
+          height={1280}
+          alt=''
+          className={styles.photoNumberTwo}
+        />
+        <Image
+          src={`/album/${name}/${name}3.jpg`}
+          width={1280}
+          height={1280}
+          alt=''
+          className={styles.photoNumberThree}
+        />
+      </div>
+    </>
   );
 }
